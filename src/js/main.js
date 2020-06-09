51795a16004083c3ab5d4e5bb3197831da745b66
hoangviethung <hoangviet.hung.009@gmail.com>
2020-06-09 09:56:43 +0700

    section-6

--- src/js/main.js
+++ src/js/main.js
@@ -3,16 +3,39 @@ import {
 	Loading
 } from './util/utilities';
 import Cookie from './lib/Cookie';
+import Swiper from 'swiper';
+
+const setBackgroundByAttr = () => {
+	const items = document.querySelectorAll('[data-bg]');
+
+	items.forEach((item) => {
+		item.style.backgroundImage = `url(${item.getAttribute('data-bg')})`;
+	})
+}
+
+const sliderIndex__2 = () => {
+	const sliderIndex__2 = new Swiper('.slider-main-index__2 .swiper-container', {
+		speed: 1000,
+		// autoplay: {
+		// 	delay: 3000,
+		// 	disableOnInteraction: false,
+		// },
+		navigation: {
+			nextEl: '.slider-main-index__2 .swiper-button-next',
+			prevEl: '.slider-main-index__2 .swiper-button-prev',
+		},
+	})
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 	Cookie();
 	getSVGs();
 	Loading();
+	// Set Background By Attr
+	setBackgroundByAttr();
+	// SLIDER INDEX 2
+	sliderIndex__2();
 });
-
-document.addEventListener('DOMContentLoaded', () => {});
-
-
 
 // CHECK FORM VALID
 
